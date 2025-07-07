fbf605175190d513b07a2630dc28d7060bab135a
Sacha Hibon <sachaedgar@yahoo.fr>
2025-07-07 20:31:59 +0200

    feat: button and logs

--- src/frontend/main.py
+++ src/frontend/main.py
@@ -5,3 +5,13 @@ TITLE = "Hello Wolrd !!!"
 st.set_page_config(page_title=TITLE, layout="centered")
 
 st.title(TITLE)
+
+
+if "app_just_started" not in st.session_state:
+    st.session_state.logs = ["started"]
+    st.session_state.app_just_started = True
+else:
+    st.session_state.logs.append(f"{len(st.session_state.logs)+1} connections")
+
+st.text(body=st.session_state.logs)
+st.button(label="fake")
